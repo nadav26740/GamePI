59016bbdc2ffdfecf73ebd3fb30229c7d74c1bb8
nadav26740 <nadav26740@gmail.com>
2024-12-27 16:54:27 +0200

    QOL Update to the build

--- src/build.sh
+++ src/build.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 set -e # stop on error
-OUTFILE="sfml-app.out"
-COMPILING_MODE_HEADER="CurrentMode.hpp"
 
-g++ -std=c++17 -c main.cpp
+# output file
+OUTFILE="sfml-app.out"
+
+# for macro flags 
+COMPILING_MODE_HEADER="CurrentMode.hpp"
 
 if [ "$*" == "--release" ]
 then
-    echo "#define RELEASE" > $COMPILING_MODE_HEADER
     echo buliding on Release Mode
+    echo "#define RELEASE" > $COMPILING_MODE_HEADER
+    mkdir -p release
+    
+    # Creating the object files
+    g++ -std=c++17 -c main.cpp -o release/main.o
+    
     sleep 0.5
-    g++ main.o -O3 $OUTFILE -lsfml-graphics -lsfml-window -lsfml-system
+    g++ release/main.o -O3 "release/$OUTFILE" -lsfml-graphics -lsfml-window -lsfml-system
+    
 else
-    echo "#define DEBUG" > $COMPILING_MODE_HEADER
     echo Building on debug mode
+    echo "#define DEBUG" > $COMPILING_MODE_HEADER
+    mkdir -p debug
+    
+    # Creating the object files
+    g++ -std=c++17 -c main.cpp -o debug/main.o
+    
     sleep 0.5
-    g++ main.o -o $OUTFILE -lsfml-graphics -lsfml-window -lsfml-system
+    g++ debug/main.o -o "debug/$OUTFILE" -lsfml-graphics -lsfml-window -lsfml-system
 fi
 
 echo Compiled into sfml-app.out
