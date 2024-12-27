45ff3500706dc5ff9acda825e580ac337f33276c
nadav26740 <nadav26740@gmail.com>
2024-12-27 23:26:25 +0200

    Icon Sprite now exists

--- src/main.cpp
+++ src/main.cpp
@@ -3,55 +3,69 @@
 #include <SFML/Graphics.hpp>
 #include "CurrentMode.hpp"
 
+#define ICON_PATH "../../assets/GamePIicon.png"
+
 #define GET_TIME std::chrono::time_point_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now()).time_since_epoch()
 
 int main()
 {
-    
+
     sf::RenderWindow window(sf::VideoMode({640, 480}), "SFML works!");
     sf::CircleShape shape(100.0f);
-    shape.setFillColor(sf::Color::Green);
+    sf::Texture icon_texture;
+    sf::Sprite icon_sprite;
+
+    if (!icon_texture.loadFromFile(ICON_PATH))
+    {
+        std::cerr << "Failed to load icon Texture" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    icon_texture.setSmooth(false);
     
-    window.setPosition({0,0});
+    icon_sprite.setTexture(icon_texture);
+    icon_sprite.scale(4.0f, 4.0f);
+    icon_sprite.setPosition(120, 120);
+    //shape.setFillColor(sf::Color::Green);
+    window.setPosition({0, 0});
+    
 
     while (window.isOpen())
     {
-        sf::Event event; // event varibale 
-        while(window.pollEvent(event)) // getting the current event
+        sf::Event event;                // event varibale
+        while (window.pollEvent(event)) // getting the current event
         {
             // getting the Current event
-        #ifdef DEBUG
+#ifdef DEBUG
             auto event_start_pt = GET_TIME;
-        #endif
+#endif
 
             switch (event.type)
             {
-                case sf::Event::Closed:
+            case sf::Event::Closed:
 
-                    window.close();
-                    break;
-                
-                case sf::Event::KeyPressed:
-                    if (event.key.code == sf::Keyboard::Space)
-                    {
-                        shape.setRadius(70.0f);
-                    }
+                window.close();
+                break;
 
-                default:
-                    break;
+            case sf::Event::KeyPressed:
+                if (event.key.code == sf::Keyboard::Space)
+                {
+                    shape.setRadius(70.0f);
+                }
+
+            default:
+                break;
             }
 
-        #ifdef DEBUG
+#ifdef DEBUG
             std::cout << "[(main.cpp) DEBUG] Event Calculation time: " << GET_TIME.count() - event_start_pt.count() << "ms" << std::endl;
-        #endif
-                
-
+#endif
         }
 
         // rendering the window
         window.clear();
-        window.draw(shape);
+        window.draw(icon_sprite);
+        //window.draw(shape);
         window.display();
-        
     }
 }
