5e99982b29cdf6346e1c4265c33d840f517ef6c3
nadav26740 <nadav26740@gmail.com>
2024-12-27 20:43:03 +0200

    QOL build update

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -e # stop on error
+
 echo Installing dependencies
 
 sudo apt upgrade
--- src/build.sh
+++ src/build.sh
@@ -1,11 +1,48 @@
 #!/bin/bash
 set -e # stop on error
 
+# files to compile
+CODE_FILES=(
+    "main.cpp"
+    "Tester.cpp"
+)
+
+OBJECTS_PATH=()
+
 # output file
 OUTFILE="sfml-app.out"
 
 # for macro flags 
 COMPILING_MODE_HEADER="CurrentMode.hpp"
+
+# assembling all the code files to object files
+AssembleAllFiles()
+{
+    folder_location=$1
+    for code_file in ${CODE_FILES[@]}
+    do
+        echo Assembling $code_file
+        g++ -std=c++17 -c $code_file -o ${folder_location}/${code_file}.obj
+        OBJECTS_PATH+=(${folder_location}/${code_file}.obj) # saving the asmbled files
+    done
+    echo Object Files Created: ${OBJECTS_PATH[@]}
+}
+
+# checking that all files exists
+Integrity_check()
+{
+    echo Integrity Check
+    for file_path in ${CODE_FILES[@]}
+    do
+        if [ ! -e $file_path ]
+        then
+            echo \[ERROR\] $file_path Not Exists! 
+            exit -1
+        fi
+    done
+}
+
+Integrity_check
 
 if [ "$*" == "--release" ]
 then
@@ -14,21 +51,21 @@ then
     mkdir -p release
     
     # Creating the object files
-    g++ -std=c++17 -c main.cpp -o release/main.o
+    AssembleAllFiles release
     
     sleep 0.5
-    g++ release/main.o -O3 "release/$OUTFILE" -lsfml-graphics -lsfml-window -lsfml-system
+    g++ ${OBJECTS_PATH[@]} -O3 -lsfml-graphics -lsfml-window -lsfml-system -o "release/$OUTFILE" 
     
 else
-    echo Building on debug mode
+    echo Building on Debug mode
     echo "#define DEBUG" > $COMPILING_MODE_HEADER
     mkdir -p debug
     
     # Creating the object files
-    g++ -std=c++17 -c main.cpp -o debug/main.o
+    AssembleAllFiles debug
     
     sleep 0.5
-    g++ debug/main.o -o "debug/$OUTFILE" -lsfml-graphics -lsfml-window -lsfml-system
+    g++ ${OBJECTS_PATH[@]} -lsfml-graphics -lsfml-window -lsfml-system -o "debug/$OUTFILE" 
 fi
 
 echo Compiled into sfml-app.out
