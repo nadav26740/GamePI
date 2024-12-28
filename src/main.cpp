2921b0cb4b4bc53d3492370238a7c5e5e4c21aac
nadav26740 <nadav26740@gmail.com>
2024-12-28 13:14:57 +0200

    Fixed Miss function call

--- src/main.cpp
+++ src/main.cpp
@@ -25,7 +25,7 @@ int main()
     
     icon_sprite.setTexture(icon_texture);
     icon_sprite.scale(4.0f, 4.0f);
-    SFMLBoost::CenterASprite(&icon_sprite, &icon_texture, &window);
+    SFMLBoost::CenterASprite(&icon_sprite, &window);
 
 
     // icon_sprite.setOrigin({icon_texture.getSize().x / 2.0f, icon_texture.getSize().y / 2.0f});
