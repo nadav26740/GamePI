6d5608555cf646a39b58a073cd157e4d2cb44500
Nadav M <nadav26740@gmail.com>
2025-01-17 20:09:13 +0200

    AssetsCacheManager update functions became virtual and status has been added [WIP]

--- src/Core/AssetsCacheManager.hpp
+++ src/Core/AssetsCacheManager.hpp
@@ -25,24 +25,46 @@ namespace SOUND_NAMES {
 /// Singleton
 class AssetsCacheManager
 {
-private:
+protected:
+    /// @brief Cache map of all the textures loaded
     std::map<std::string, sf::Texture> m_Texture_map;
+
+    /// @brief Cache map of all the sounds loaded
     std::map<std::string, sf::SoundBuffer> m_SoundBuffer_map;
     AssetsCacheManager();
+
+    // [Singleton] too make sure no one can create another object of this type
     AssetsCacheManager(AssetsCacheManager const&) = delete;       // Don't Implement
     void operator=(AssetsCacheManager const&) = delete;    // Don't implement
 
+    std::vector<std::string> Failed_To_Load;
+    std::vector<std::string> json_loaded;
+
 public:    
+    struct AssetsCacheStatus {
+        unsigned int textures_loaded;
+        unsigned int Sounds_loaded;
+
+        std::vector<std::string> Loaded_assets;
+        std::vector<std::string> Failed_To_Load;
+    
+        std::vector<std::string> json_loaded;
+
+        // inline std::ostream& operator<<(std::ostream& os, const AssetsCacheManager& cache);
+    };
+
     /// @brief Too allow singleton we used this function to create single static object of this class
     /// @return the Intance of the current AssetsCache
     static AssetsCacheManager* GetIntance();
 
     // TODO: Load Assets From jsonfile (Like texture pack [Theme])
-    void LoadFromJson(const std::string& path /* = DEFAULT_ASSETS_JSON_PATH */);
+    virtual void LoadFromJson(const std::string& path /* = DEFAULT_ASSETS_JSON_PATH */);
     
     // TODO: Get Soundbuffer pointer from the assets cache
-    const sf::SoundBuffer* GetSoundBuffer(const std::string &name);
+    virtual const sf::SoundBuffer* GetSoundBuffer(const std::string &name);
     
     // TODO: Get Texture pointer from the assets cache
-    const sf::Texture* GetTexture(const std::string &name);
+    virtual const sf::Texture* GetTexture(const std::string &name);
+
+    // TODO: Implement a status 
 };
